e2a6d73ca784475bb1d5eb643b5e2da21e6b0873
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2024-03-25 12:22:07 +0100

    installed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aaliyyah/phpiggy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4574e656c9585e38f85eb7257d3461b44b9f405c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aaliyyah/phpiggy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4574e656c9585e38f85eb7257d3461b44b9f405c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
